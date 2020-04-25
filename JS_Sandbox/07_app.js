d0d711d4fdfca3d1a2f89a15f7e32f923f0367cc
aka2029 <asaks22@gmail.com>
2020-04-26 00:10:11 +0530

    new commit

---
+++ JS_Sandbox/07_app.js
@@ -0,0 +1,39 @@
+// ARRAYS & ARRAYS METHODS
+
+// arrays can be mutated(ie can be modified) and iterated though & can be used in some really complex algorithms, now create some arrays...
+
+const numbers = [43, 56, 33, 23, 44, 36, 5];
+const numbers2 = new Array(22, 45, 33, 76, 54);
+const fruit = ["Apple", "Banana", "Orange", "Pear"];
+const mixed = [22, "Hello", true, undefined, null, { a: 1, b: 1 }, new Date()];
+
+let val;
+
+// get array length
+val = numbers.length;
+// check if is array
+val = Array.isArray(numbers); //very useful while dealing with DOM
+// get single value
+val = numbers[3];
+val = numbers[0];
+// Insert into array
+numbers[2] = 100;
+// Find index of value
+val = numbers.indexOf(36);
+
+// MUTATING ARRAYS
+// Add on to end
+numbers.push(250);
+// Add on to front
+numbers.unshift(120);
+// Take off from end
+numbers.pop();
+// Take off from front
+numbers.shift();
+// Splice values
+numbers.splice(1, 3); //we're telling where we wanna start & where to end(end is included)
+// Reverse
+numbers.reverse();
+
+console.log(numbers);
+console.log(val);
