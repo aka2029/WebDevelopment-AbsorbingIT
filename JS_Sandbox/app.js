3a6b65facca05019d2a1c92f8e327eda80f8fa64
aka2029 <asaks22@gmail.com>
2020-04-25 13:08:31 +0530

    new commit

---
+++ JS_Sandbox/app.js
@@ -0,0 +1,30 @@
+// Log to console -> The console. log() is a function in JavaScript which is used to print any kind of variables defined before in it or to just print any message that needs to be displayed to the user.
+
+console.log("Hello World");
+console.log(123);
+console.log(true);
+var greeting = "Hello";
+console.log(greeting);
+
+// Arrays
+console.log([1, 2, 3, 4]);
+
+// objects
+console.log({ a: 1, b: 2 });
+
+console.table({ a: 1, b: 2 });
+
+console.error("This is some error");
+
+console.clear(); //This clears the console
+
+console.warn("This is a warning");
+
+console.time("Hello"); //Start Time
+console.log("Hello World");
+console.log("Hello World");
+console.log("Hello World");
+console.log("Hello World");
+console.log("Hello World");
+console.log("Hello World");
+console.timeEnd("Hello"); //End Time
