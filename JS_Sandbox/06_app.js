612ef61b98952b079f79e9327137afa052e17ad9
aka2029 <asaks22@gmail.com>
2020-04-25 23:47:43 +0530

    new commit

---
+++ JS_Sandbox/06_app.js
@@ -0,0 +1,56 @@
+// TEMPLATE LITERALS - part of ES6 but are compatible with all browsers
+
+const name = "John";
+const age = 30;
+const job = "Web Developer";
+const city = "Miami";
+
+// -> a lot of times you're gonna be inserting HTML from JS. so when you fetch something with AJAX and want to putin an unordered list or a table with dynamic data.. then we'll be doing it with js. so, we'll have to create some kind of html string with html and with the dynamic data that you want to insert
+
+let html;
+
+// Without template strings (es5)
+html =
+  "<ul><li>Name: " +
+  name +
+  "</li><li>Age: " +
+  age +
+  " </li><li>Job " +
+  job +
+  " </li><li>City: " +
+  city +
+  " </li></ul>";
+
+html =
+  "<ul>" +
+  "<li>Name: " +
+  name +
+  "</li>" +
+  "<li>Age: " +
+  age +
+  "</li>" +
+  "<li>Job: " +
+  job +
+  "</li>" +
+  "<li>City: " +
+  city +
+  "</li>";
+
+function hello() {
+  return "hello";
+}
+
+// With template strings (ES6) -> use backticks ``, this is how it knows that it is a template string
+html = `
+  <ul>
+    <li>Name: ${name}</li>
+    <li>Age: ${age}</li>
+    <li>Job: ${job}</li>
+    <li>City: ${city}</li>
+    <li>${2 + 2}</li>
+    <li>${hello()}</li>
+    <li>${age > 30 ? "Over 30" : "Under 30"}</li>
+  </ul>
+`;
+
+document.body.innerHTML = html; //This is the part of the DOM
