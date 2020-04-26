54736e28ae07cdf152abb81a8ea6c4fe9e81029d
aka2029 <asaks22@gmail.com>
2020-04-26 16:52:34 +0530

    new commit

--- JS_Sandbox/13_app.js
+++ JS_Sandbox/13_app.js
@@ -32,9 +32,68 @@ console.clear();
 
 // --------------------------------------------------------
 
-// DO WHILE
+// DO WHILE -> its always gonna run no matter what
 let j = 0;
 do {
   console.log("Number " + j);
   j++;
 } while (j < 10);
+
+// --------------------------------------------------------
+
+console.clear();
+
+// Arrays have looping method that are suggested over for loop or something
+const cars = ["Ford", "Chevy", "Honda", "Toyota"];
+
+// LOOP THROUGH ARRAY
+for (let i = 0; i < cars.length; i++) {
+  console.log(cars[i]);
+}
+
+console.clear();
+
+// FOR EACH ARRAY LOOP  - this is suggested if you're trying through an array for some reason
+cars.forEach(function(car) {
+  console.log(car);
+});
+
+cars.forEach(function(car, index) {
+  console.log(`${index} : ${car}`);
+});
+
+cars.forEach(function(car, index, array) {
+  console.log(`${index} : ${car}`);
+  console.log(array);
+});
+
+console.clear();
+
+// ---------------------------------------------------------
+
+// MAP -> used to return something different so, to return a different array
+const users = [
+  { id: 1, name: "John" },
+  { id: 2, name: "Sara" },
+  { id: 3, name: "Karen" },
+  { id: 4, name: "Steve" }
+];
+
+const ids = users.map(function(user) {
+  return user.id;
+});
+
+console.log(ids);
+
+// --------------------------------------------------------
+
+// FOR IN LOOP
+const user = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 40
+};
+
+for (let x in user) {
+  console.log(`${x} : ${user[x]}`);
+}
