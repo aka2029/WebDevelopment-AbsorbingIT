ea385c8735cfdbec61f7620ba7c44a29637130c0
aka2029 <asaks22@gmail.com>
2020-04-28 23:38:40 +0530

    new commit

---
+++ JS_Sandbox2/02_app.js
@@ -0,0 +1,5 @@
+/* After this modern JS, jQuery is never suggested as it is unnecessary most of the time, it adds bloat to the application.
+-> one things that jQuery is good for is quick simple plugins or scripts that you need to do a specific action but never for DOM manipulation
+ */
+
+// DIFFERENT SELECTORS - COUPLE ELEMENT & SINGLE ELEMENT SELECTORS
