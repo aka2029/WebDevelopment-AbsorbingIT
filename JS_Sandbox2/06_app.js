beb33921debf54188129dee0689d1962c10cf44c
aka2029 <asaks22@gmail.com>
2020-04-30 10:39:46 +0530

    new commit

---
+++ JS_Sandbox2/06_app.js
@@ -0,0 +1,53 @@
+// REMOVING AND REPLACING ELEMENTS, HOW TO MANIPULATE CLASSES AND ATTRIBUTES
+
+/*  */
+// create an element
+const newHeading = document.createElement("h2");
+
+// Add id
+newHeading.id = "task-title";
+// New text node
+newHeading.appendChild(document.createTextNode("Task List"));
+
+// Get the old heading
+const oldHeading = document.getElementById("task-title");
+// Parent
+const cardAction = document.querySelector(".card-action");
+
+// Replace -> final step, how we replace something
+cardAction.replaceChild(newHeading, oldHeading);
+
+// ----------------------------------------------------------------------
+
+// Remove Element
+const lis = document.querySelectorAll("li");
+const list = document.querySelector("ul");
+
+// Remove list item
+lis[0].remove();
+
+// Remove child element
+list.removeChild(lis[2]);
+
+// Classes & Attributes
+const firstLi = document.querySelector("li:first-child");
+const link = firstLi.children[0];
+
+let val;
+
+val = link.className;
+val = link.classList;
+val = link.classList[0];
+link.classList.add("test");
+link.classList.remove("test");
+val = link;
+
+// Attributes
+val = link.getAttribute("href");
+val = link.setAttribute("href", "http://google.com");
+link.setAttribute("title", "Google");
+val = link.hasAttribute("title");
+link.removeAttribute("title");
+val = link;
+
+console.log(val);
