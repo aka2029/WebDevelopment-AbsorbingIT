f5e811d427e444e73d1d05676348d0638a6087fc
aka2029 <asaks22@gmail.com>
2020-04-30 18:31:18 +0530

    new commit

--- JS_Sandbox2/11_app.js
+++
@@ -1,3 +0,0 @@
-// LOCAL AND SESSION STORAGE
-
-// this series i practiced on dom but had to do a small part of storage bec i'll be using it for some project
---
+++ JS_Sandbox2/11_storage.js
@@ -0,0 +1,3 @@
+// LOCAL AND SESSION STORAGE
+
+// this series i practiced on dom but had to do a small part of storage bec i'll be using it for some project, i'll persist it to local storage so, that when i close and come back the tasks are still there
