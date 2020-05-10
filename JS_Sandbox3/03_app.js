c432f6d9d0d7623091683303cbd441096869e7ae
aka2029 <asaks22@gmail.com>
2020-05-10 23:53:34 +0530

    new commit

---
+++ JS_Sandbox3/03_app.js
@@ -0,0 +1,4 @@
+// PROTOTYPES - One of the most difficult topics in JS, each object in js has a prototype and a prototype is an object itself, all objects inherit their methods and properties from their prototype
+
+// Object.prototype - when dealing with obj literals
+// Person.prototype - when dealing with obj ceated with constructors
