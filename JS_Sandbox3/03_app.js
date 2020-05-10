c7d6fd7397cdc380a6e0809039fd43950b629e35
aka2029 <asaks22@gmail.com>
2020-05-11 00:09:29 +0530

    new commit

--- JS_Sandbox3/03_app.js
+++ JS_Sandbox3/03_app.js
@@ -2,3 +2,49 @@
 
 // Object.prototype - when dealing with obj literals
 // Person.prototype - when dealing with obj ceated with constructors
+
+// Person constructor
+function Person(firstName, lastName, dob) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.birthday = new Date(dob);
+
+  // this.calculateAge = function() {
+  //   const diff = Date.now() - this.birthday.getTime();
+  //   const ageDate = new Date(diff);
+  //   return Math.abs(ageDate.getUTCFullYear() - 1970);
+  // };
+}
+
+// Calculate age
+Person.prototype.calculateAge = function() {
+  const diff = Date.now() - this.birthday.getTime();
+  const ageDate = new Date(diff);
+  return Math.abs(ageDate.getUTCFullYear() - 1970);
+};
+
+// Get full name
+Person.prototype.getFullName = function() {
+  return `${this.firstName} ${this.lastName}`;
+};
+
+// Gets Married
+Person.prototype.getsMarried = function(newLastName) {
+  this.lastName = newLastName;
+};
+
+const john = new Person("John", "Doe", "8-12-90");
+const mary = new Person("Mary", "Johnson", "March 20 1978");
+
+console.log(mary);
+
+console.log(john.calculateAge());
+
+console.log(mary.getFullName());
+
+mary.getsMarried("Smith");
+
+console.log(mary.getFullName());
+
+console.log(mary.hasOwnProperty("firstName")); //simple syntax
+console.log(mary.hasOwnProperty("getFullName")); //false but above is true
