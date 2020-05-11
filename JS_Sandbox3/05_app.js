aa818e17a4235a1527bdd4d136d6179fb58f8a69
aka2029 <asaks22@gmail.com>
2020-05-11 13:15:43 +0530

    new commit

--- JS_Sandbox3/05_app.js
+++ JS_Sandbox3/05_app.js
@@ -1,9 +1,13 @@
 // USING Object.create - we're gonna be able to create prototypes inside of a parent object and have different properties with different prototype methods or different prototype functions
 
 const personPrototypes = {
-  //personPrototype is a variable which is made a object
+  //personPrototype is a variable which is made a object. And this as a whole becomes a prototype function and has various prototype methods
   greeting: function() {
     return `Hello there ${this.firstName} ${this.lastName}`;
+  },
+
+  getsMarried: function(newLastName) {
+    this.lastName = newLastName;
   }
 };
 
@@ -12,4 +16,17 @@ mary.firstName = "Mary";
 mary.lastName = "Williams";
 mary.age = 30;
 
-console.log(mary);
+mary.getsMarried("Thompson");
+
+console.log(mary.greeting());
+
+// below we are trying another syntax than above
+const akash = Object.create(personPrototypes, {
+  firstName: { value: "Akash" },
+  lastName: { value: "Sharma" },
+  age: { value: 21 }
+});
+
+console.log(akash);
+
+console.log(akash.greeting());
