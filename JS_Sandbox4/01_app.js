14a6156caff44e6e79968194d3bb7c6f3981a3a3
aka2029 <asaks22@gmail.com>
2020-05-11 23:32:33 +0530

    new commit

--- JS_Sandbox4/01_app.js
+++ JS_Sandbox4/01_app.js
@@ -1,5 +1,6 @@
 // XHR Object methods & working with Test
 // Ajax and xhr are more complicated than fetch
+// will only work with live server
 
 document.getElementById("button").addEventListener("click", loadData);
 
@@ -7,7 +8,7 @@ function loadData() {
   // Create an XHR Object
   const xhr = new XMLHttpRequest(); // just as we would use any constructor for an object
 
-  // OPEN - one of the methods/properties
+  // OPEN - one of the methods/properties - get is to read the file, then comes file name
   xhr.open("GET", "data.txt", true);
 
   xhr.onload = function() {
@@ -18,6 +19,13 @@ function loadData() {
   };
 
   xhr.send();
+
+  // readyState Values
+  //     0: request not initialized
+  //     1: sever connection established
+  //     2: request received
+  //     3: processing request
+  //     4: request finished and response is ready
 
   // HTTP Statuses - these are not all but the common ones
   // 200: "OK"
