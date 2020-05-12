90205333363bec8b0b34caef1c5f583123334b47
aka2029 <asaks22@gmail.com>
2020-05-12 19:07:07 +0530

    new commit

--- JS_Sandbox4/07_app.js
+++ JS_Sandbox4/07_app.js
@@ -4,6 +4,11 @@
 
 document.getElementById("button1").addEventListener("click", getText);
 
+document.getElementById("button2").addEventListener("click", getJson);
+
+document.getElementById("button3").addEventListener("click", getExternal);
+
+// Get local text file
 function getText(e) {
   // remember fetch returns promises so, have to use .then
   fetch("test.txt")
@@ -21,6 +26,55 @@ function getText(e) {
 
     .catch(function(err) {
       //for catching some error if exists eg. test1.txt
+      console.log(err);
+    });
+
+  e.preventDefault();
+}
+
+// Get local json data
+function getJson(e) {
+  fetch("posts.json")
+    .then(function(res) {
+      return res.json();
+    })
+
+    .then(function(data) {
+      console.log(data);
+      let output = "";
+
+      data.forEach(function(post) {
+        output += `<li>${post.title}</li>`;
+      });
+      document.getElementById("output").innerHTML = output;
+    })
+
+    .catch(function(err) {
+      console.log(err);
+    });
+
+  e.preventDefault();
+}
+
+// Get from external API
+function getExternal(e) {
+  fetch("https://api.github.com/users")
+    .then(function(res) {
+      return res.json();
+    })
+
+    .then(function(data) {
+      console.log(data);
+
+      let output = "";
+
+      data.forEach(function(user) {
+        output += `<li>${user.login}</li>`;
+      });
+      document.getElementById("output").innerHTML = output;
+    })
+
+    .catch(function(err) {
       console.log(err);
     });
 
