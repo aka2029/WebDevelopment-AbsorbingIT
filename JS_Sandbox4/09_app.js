1e176c23a3700c3d8776288f8bba3a7b32ea8730
aka2029 <asaks22@gmail.com>
2020-05-13 00:24:25 +0530

    new commit

--- JS_Sandbox4/09_app.js
+++ JS_Sandbox4/09_app.js
@@ -30,3 +30,17 @@ myFunc()
 .catch(err => console.log(err));
 
 */
+
+// Eg. USING FETCH() - which is by the way the best wasy to use them
+async function getUsers() {
+  // await response of the fetch call
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+
+  // Only proceed once its resolved
+  const data = await response.json();
+
+  // Only proceed once second promise is resolved
+  return data;
+}
+
+getUsers().then(users => console.log(users));
