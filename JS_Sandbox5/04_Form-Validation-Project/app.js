1bd3dc62e45f40ed0f8e6f88042050a377707bde
aka2029 <asaks22@gmail.com>
2020-05-17 10:22:29 +0530

    new commit

--- JS_Sandbox5/04_Form-Validation-Project/app.js
+++ JS_Sandbox5/04_Form-Validation-Project/app.js
@@ -1,4 +1,4 @@
-// Form Blur Event Listener
+// Form Blur Event Listeners
 document.getElementById("name").addEventListener("blur", validateName);
 document.getElementById("zip").addEventListener("blur", validateZip);
 document.getElementById("email").addEventListener("blur", validateEmail);
@@ -6,7 +6,7 @@ document.getElementById("phone").addEventListener("blur", validatePhone);
 
 function validateName() {
   const name = document.getElementById("name");
-  const re = /^[A-za-z]{2,10}$/;
+  const re = /^[a-zA-Z]{2,10}$/;
 
   if (!re.test(name.value)) {
     name.classList.add("is-invalid");
@@ -15,8 +15,35 @@ function validateName() {
   }
 }
 
-function validateZip() {}
+function validateZip() {
+  const zip = document.getElementById("zip");
+  const re = /^[0-9]{5}(-[0-9]{4})?$/;
 
-function validateEmail() {}
+  if (!re.test(zip.value)) {
+    zip.classList.add("is-invalid");
+  } else {
+    zip.classList.remove("is-invalid");
+  }
+}
 
-function validatePhone() {}
+function validateEmail() {
+  const email = document.getElementById("email");
+  const re = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
+
+  if (!re.test(email.value)) {
+    email.classList.add("is-invalid");
+  } else {
+    email.classList.remove("is-invalid");
+  }
+}
+
+function validatePhone() {
+  const phone = document.getElementById("phone");
+  const re = /^\(?\d{3}\)?[-. ]?\d{3}[-. ]?\d{4}$/;
+
+  if (!re.test(phone.value)) {
+    phone.classList.add("is-invalid");
+  } else {
+    phone.classList.remove("is-invalid");
+  }
+}
