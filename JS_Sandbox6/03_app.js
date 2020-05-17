e8d67a922814c8f7db1ea60229e7087f24ead402
aka2029 <asaks22@gmail.com>
2020-05-17 14:14:40 +0530

    new commit

---
+++ JS_Sandbox6/03_app.js
@@ -0,0 +1,4 @@
+// SYMBOLS IN Es6 - the imp thing about symbols is that each and every one of them is very unique. This makes them very valuable in terms of obj prop identifier. this is the main purpose of symbols is their uniqueness.
+
+// Create a symbol : symbol is not a constructor, it's an actual primitive value so, cannt write it like "new Symbol()"
+const sym1 = Symbol();
