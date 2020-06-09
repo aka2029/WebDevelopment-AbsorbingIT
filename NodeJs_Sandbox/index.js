b8a79564404da07034e1ca1b0c9eeb1114ee811d
aka2029 <asaks22@gmail.com>
2020-06-09 18:19:20 +0530

    new commit

--- NodeJs_Sandbox/index.js
+++ NodeJs_Sandbox/index.js
@@ -94,6 +94,30 @@ const server = http.createServer((request, response) => {
       contentType = "image/jpg";
       break;
   }
+
+  // Read file
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      if (err.code == "ENOENT") {
+        // Page not found
+        fs.readFile(
+          path.join(__dirname, "public", "404.html"),
+          (err, content) => {
+            response.writeHead(200, { "Content-Type": "text/html" });
+            response.end(content, "utf8");
+          }
+        );
+      } else {
+        // Some server error
+        response.writeHead(500);
+        response.end(`Server Error: ${err.code}`);
+      }
+    } else {
+      // Success
+      response.writeHead(200, { "Content-Type": contentType });
+      response.end(content, "utf8");
+    }
+  });
 });
 
 const PORT = process.env.PORT || 5000;
